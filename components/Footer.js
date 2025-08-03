ac0d15a1492c7f837ff167cd9949cb8aa28e99c2
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-03 12:06:09 +0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#37474F] text-white text-center py-4 mt-8 shadow-inner">
+      <p>© {new Date().getFullYear()} ForgeTomorrow. All rights reserved.</p>
+    </footer>
+  );
+}
